55ddd687fe84f7bf4147909553d8e8c6b55236e2
Jaemin Oh <ojm6135@naver.com>
2025-09-16 22:33:21 +0900

    build: 초기 설정

---
+++ src/main/java/com/waggle/wagglex2/Wagglex2Application.java
@@ -0,0 +1,13 @@
+package com.waggle.wagglex2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wagglex2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wagglex2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/waggle/wagglex2/Wagglex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waggle.wagglex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wagglex2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
