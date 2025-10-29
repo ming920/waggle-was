b690c3f248f034fd13dec946397b1ba7d7430130
Jaemin Oh <ojm6135@naver.com>
2025-10-29 21:53:46 +0900

    feat: Application 엔티티 생성

---
+++ src/main/java/com/wagglex2/waggle/domain/application/entity/Application.java
@@ -0,0 +1,67 @@
+package com.wagglex2.waggle.domain.application.entity;
+
+import com.wagglex2.waggle.domain.application.type.ApplicationStatus;
+import com.wagglex2.waggle.domain.common.entity.BaseRecruitment;
+import com.wagglex2.waggle.domain.common.type.PositionType;
+import com.wagglex2.waggle.domain.common.type.Skill;
+import com.wagglex2.waggle.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Table(name = "applications")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Application {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "applicant_id", referencedColumnName = "id", nullable = false)
+    private User applicant;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recruitment_id", referencedColumnName = "id", nullable = false)
+    private BaseRecruitment recruitment;
+
+    @Column(nullable = false)
+    private String content;
+
+    /**
+     * Project 공고에 대한 지원에만 존재<br>
+     * 그 외의 경우에는 null
+     */
+    @Enumerated(EnumType.STRING)
+    private PositionType position;
+
+    /**
+     * Project 공고에 대한 지원에만 존재<br>
+     * 그 외의 경우에는 empty set
+     */
+    @Enumerated(EnumType.STRING)
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(
+            name = "application_skills",
+            joinColumns = @JoinColumn(name = "application_id", referencedColumnName = "id", nullable = false)
+    )
+    private Set<Skill> skills = new HashSet<>();
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ApplicationStatus status = ApplicationStatus.SUBMITTED;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
