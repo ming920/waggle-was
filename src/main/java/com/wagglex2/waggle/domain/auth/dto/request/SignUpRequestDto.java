32f9ed00fc180e3bd064d8c9d7b015a52343528c
ming920 <rkfsky920@gmail.com>
2025-10-30 01:42:44 +0900

    refactor: 회원정보 수정 애노테이션 수정 및 추가

--- src/main/java/com/wagglex2/waggle/domain/auth/dto/request/SignUpRequestDto.java
+++ src/main/java/com/wagglex2/waggle/domain/auth/dto/request/SignUpRequestDto.java
@@ -8,7 +8,6 @@ import com.wagglex2.waggle.domain.user.entity.User;
 import com.wagglex2.waggle.domain.user.entity.type.University;
 import com.wagglex2.waggle.domain.user.entity.type.UserRoleType;
 import jakarta.validation.constraints.*;
-import org.hibernate.validator.constraints.Range;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Set;
@@ -37,17 +36,19 @@ public record SignUpRequestDto(
         String email,
 
         @NotNull(message = "학년이 누락되었습니다.")
-        @Range(min = 1, max = 4, message = "모집 학년은 1 이상 4 이하여야 합니다.")
+        @Min(value = 1, message = "학년은 1 이상이어야 합니다.")
+        @Max(value = 4, message = "학년은 4 이하여여야 합니다.")
         Integer grade,
 
         @NotNull(message = "포지션이 누락되었습니다.")
         PositionType position,
 
         @NotEmpty(message = "기술 스택이 누락되었습니다.")
+        @Size(max = 10, message = "기술 스택은 최대 10개까지 선택할 수 있습니다.")
         Set<Skill> skills,
 
         @NotBlank(message = "한 줄 소개가 누락되었습니다.")
-        @Size(max = 255, message = "한 줄 소개는 100자를 초과할 수 없습니다.")
+        @Size(max = 100, message = "한 줄 소개는 100자를 초과할 수 없습니다.")
         String shortIntro
 ) {
     public SignUpRequestDto {
--- src/main/java/com/wagglex2/waggle/domain/user/dto/request/UserUpdateRequestDto.java
+++ src/main/java/com/wagglex2/waggle/domain/user/dto/request/UserUpdateRequestDto.java
@@ -3,7 +3,6 @@ package com.wagglex2.waggle.domain.user.dto.request;
 import com.wagglex2.waggle.domain.common.type.PositionType;
 import com.wagglex2.waggle.domain.common.type.Skill;
 import jakarta.validation.constraints.*;
-import org.hibernate.validator.constraints.Range;
 
 import java.util.Set;
 
@@ -12,9 +11,8 @@ import java.util.Set;
  *
  * <p>PATCH /me 요청 시 클라이언트가 전달하는 데이터 구조를 정의한다.</p>
  * <ul>
- *   <li>모든 필드는 선택적으로 입력할 수 있으며(null 허용), 값이 존재할 경우에만 수정된다.</li>
+ *   <li>닉네임 필드는 선택적으로 입력할 수 있으며 나머지 필드는 필수 입력 항목이다.</li>
  *   <li>닉네임, 학년, 포지션, 보유 기술 스택, 한 줄 소개 등을 수정 가능하다.</li>
- *   <li>Bean Validation 애노테이션을 통해 클라이언트 입력에 대한 유효성 검증을 수행한다.</li>
  * </ul>
  */
 public record UserUpdateRequestDto(
@@ -22,14 +20,20 @@ public record UserUpdateRequestDto(
                 message = "닉네임은 2-10자의 영문, 한글, 숫자만 입력할 수 있습니다.")
         String nickname,
 
-        @Range(min = 1, max = 4, message = "모집 학년은 1 이상 4 이하여야 합니다.")
+        @NotNull(message = "학년은 필수 입력 항목입니다.")
+        @Min(value = 1, message = "학년은 1 이상이어야 합니다.")
+        @Max(value = 4, message = "학년은 4 이하여어야 합니다.")
         Integer grade,
 
+        @NotNull(message = "포지션은 필수 입력 항목입니다.")
         PositionType position,
 
+        @NotEmpty(message = "기술 스택은 필수 입력 항목입니다.")
+        @Size(max = 10, message = "기술 스택은 최대 10개까지 선택할 수 있습니다.")
         Set<Skill> skills,
 
-        @Size(max = 255, message = "한 줄 소개는 100자를 초과할 수 없습니다.")
+        @NotBlank(message = "한 줄 소개는 필수 입력 항목입니다.")
+        @Size(max = 100, message = "한 줄 소개는 100자를 초과할 수 없습니다.")
         String shortIntro
 ) {
 }
