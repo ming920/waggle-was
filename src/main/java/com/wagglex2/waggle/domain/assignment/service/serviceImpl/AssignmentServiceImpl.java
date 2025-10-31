2d0e7a9091650ca5cf23535b9ab8332dd57ad47d
ming920 <rkfsky920@gmail.com>
2025-10-31 23:34:50 +0900

    refactor: Assignment 생성 시 Team 및 TeamMember 추가

--- src/main/java/com/wagglex2/waggle/domain/assignment/service/serviceImpl/AssignmentServiceImpl.java
+++ src/main/java/com/wagglex2/waggle/domain/assignment/service/serviceImpl/AssignmentServiceImpl.java
@@ -9,6 +9,10 @@ import com.wagglex2.waggle.domain.assignment.entity.Assignment;
 import com.wagglex2.waggle.domain.assignment.repository.AssignmentRepository;
 import com.wagglex2.waggle.domain.assignment.service.AssignmentService;
 import com.wagglex2.waggle.domain.common.type.RecruitmentStatus;
+import com.wagglex2.waggle.domain.team.entity.Team;
+import com.wagglex2.waggle.domain.team.service.TeamService;
+import com.wagglex2.waggle.domain.team_member.entity.TeamMember;
+import com.wagglex2.waggle.domain.team_member.entity.type.TeamRole;
 import com.wagglex2.waggle.domain.user.entity.User;
 import com.wagglex2.waggle.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +27,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class AssignmentServiceImpl implements AssignmentService {
     private final AssignmentRepository assignmentRepository;
     private final UserService userService;
+    private final TeamService teamService;
 
     @Transactional
     @Override
@@ -30,7 +35,14 @@ public class AssignmentServiceImpl implements AssignmentService {
         User user = userService.findById(userId);
         Assignment newAssignment = AssignmentCreationRequestDto.toEntity(user, requestDto);
 
-        return assignmentRepository.save(newAssignment).getId();
+        Long assignmentId = assignmentRepository.save(newAssignment).getId();
+
+        Team team = new Team(newAssignment);
+        TeamMember leader = new TeamMember(team, user, TeamRole.LEADER);
+        team.addMember(leader);
+        teamService.save(team);
+
+        return assignmentId;
     }
 
     @Transactional
