de8383891ab7a0072456eb994c7a4b35f4cf3ee1
Jaemin Oh <ojm6135@naver.com>
2025-11-05 01:17:11 +0900

    refactor: NotificationResponseDto record로 변경

--- src/main/java/com/wagglex2/waggle/domain/notification/dto/response/NotificationResponseDto.java
+++ src/main/java/com/wagglex2/waggle/domain/notification/dto/response/NotificationResponseDto.java
@@ -2,47 +2,44 @@ package com.wagglex2.waggle.domain.notification.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.wagglex2.waggle.common.error.ErrorCode;
+import com.wagglex2.waggle.common.exception.BusinessException;
 import com.wagglex2.waggle.domain.application.type.ApplicationStatus;
 import com.wagglex2.waggle.domain.common.type.RecruitmentCategory;
 import com.wagglex2.waggle.domain.notification.type.NotificationType;
-import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-@Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class NotificationResponseDto {
+public record NotificationResponseDto(
+        Long notificationId,
+        Long applicationId,
+        RecruitmentCategory category,
+        String senderNickname,
+        NotificationType type,
 
-    private final Long notificationId;
-    private final Long applicationId;
-    private final RecruitmentCategory category;
-    private final String senderNickname;
-    private NotificationType type;
+        @JsonFormat(pattern = "yyyy.MM.dd")
+        LocalDateTime createdAt,
 
-    @JsonFormat(pattern = "yyyy.MM.dd")
-    private final LocalDateTime createdAt;
-
-    private final boolean isRead;
-
+        boolean isRead
+) {
     public NotificationResponseDto(
             Long notificationId, Long applicationId, RecruitmentCategory category,
             String senderNickname, ApplicationStatus applicationStatus,
             LocalDateTime createdAt, boolean isRead
     ) {
-        this.notificationId = notificationId;
-        this.applicationId = applicationId;
-        this.category = category;
-        this.senderNickname = senderNickname;
-        this.createdAt = createdAt;
-        this.isRead = isRead;
-        setType(applicationStatus);
+        this(
+            notificationId, applicationId, category, senderNickname,
+            convertStatus(applicationStatus), createdAt, isRead
+        );
     }
 
-    private void setType(ApplicationStatus applicationStatus) {
-        switch (applicationStatus) {
-            case SUBMITTED ->  this.type = NotificationType.APPLICATION_SUBMITTED;
-            case ACCEPTED -> this.type = NotificationType.APPLICATION_ACCEPTED;
-            case REJECTED -> this.type = NotificationType.APPLICATION_REJECTED;
-        }
+    private static NotificationType convertStatus(ApplicationStatus applicationStatus) {
+        return switch (applicationStatus) {
+            case SUBMITTED ->  NotificationType.APPLICATION_SUBMITTED;
+            case ACCEPTED -> NotificationType.APPLICATION_ACCEPTED;
+            case REJECTED -> NotificationType.APPLICATION_REJECTED;
+            default -> throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+        };
     }
 }
